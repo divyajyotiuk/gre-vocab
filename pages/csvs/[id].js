7bdb7cb70ec8e0e9f87fb11e05c9507248f09ff2
Divyajyoti Ukirde <divyajyoti.u@gmail.com>
2021-01-31 23:42:06 +0530

    complete and ready to host

--- pages/csvs/[id].js
+++ pages/csvs/[id].js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Router, { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import PageLayout from '../../components/pageLayout'
@@ -5,9 +6,16 @@ import { getAllCSVIds, getCSVData } from '../../lib/csvReader'
 import Card from '../../components/card'
 import BurgerMenu from '../../components/burgerMenu'
 import constants from '../../lib/constants';
+import Select from 'react-select'
 
 export default function WordsPage({paths, csvData}) {
     const data = csvData?.data;
+    const options = [
+        { value: 1, label: 'Deck 1' },
+        { value: 2, label: 'Deck 2' },
+        { value: 3, label: 'Deck 3' }
+    ];
+    const [selectedOption, setSelectedOption] = useState( { value: 1, label: 'Deck 1' });
     const updateDeck = (word, deck) => {
         console.log("here!", word, deck)
         fetch(constants.url+constants.updateDeck, {
@@ -31,10 +39,26 @@ export default function WordsPage({paths, csvData}) {
         })
         .catch((err) => {alert(err)})
     }
+
+    const handleChange = selectedOption => {
+        setSelectedOption(selectedOption);
+        console.log(`Option selected:`, selectedOption);
+    };
+
     return (
         <div className="mt-24">
+        <div className="container lg:rounded-lg mx-auto flex my-10 py-3 items-center justify-center bg-white shadow-md">
+            <div className="flex-initial mr-10 text-purple-500">Filter by Deck: </div>
+            <div className="w-44">
+                <Select 
+                    options={options}
+                    value={selectedOption}
+                    onChange={handleChange} 
+                />
+            </div>
+        </div>
         <PageLayout>
-            {Object.keys(data).length > 0 && Object.keys(data).map((word, index)=> <Card key={index} updateDeck={updateDeck} data={data[word]}></Card> )}
+            {Object.keys(data).length > 0 && Object.keys(data).filter((key)=> Number(data[key].Deck) == selectedOption.value ).map((word, index)=> <Card key={index} updateDeck={updateDeck} data={data[word]}></Card> )}
         </PageLayout>
         <BurgerMenu pages={paths} className="burger-menu" outerContainerId={"App"} ></BurgerMenu>
         </div>
