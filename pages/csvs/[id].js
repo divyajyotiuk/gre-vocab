dbaa1b243a88caab3400154cd86926a0975bcc87
Divyajyoti Ukirde <divyajyoti.u@gmail.com>
2021-01-31 21:10:57 +0530

    deck can be changed

--- pages/csvs/[id].js
+++ pages/csvs/[id].js
@@ -1,15 +1,35 @@
-import { useRouter } from 'next/router'
+import Router, { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import PageLayout from '../../components/pageLayout'
 import { getAllCSVIds, getCSVData } from '../../lib/csvReader'
 import Card from '../../components/card'
 import BurgerMenu from '../../components/burgerMenu'
+import constants from '../../lib/constants';
 
 export default function WordsPage({paths, csvData}) {
     const data = csvData?.data;
     const updateDeck = (word, deck) => {
         console.log("here!", word, deck)
-        
+        fetch(constants.url+constants.updateDeck, {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                "id": csvData.id,
+                "word": word,
+                "deck": deck
+            })
+        })
+        .then((res)=>{
+            return res.json();
+        })
+        .then((data) => {
+            if(data && data.success){
+                Router.reload(window.location.pathname);
+            }
+        })
+        .catch((err) => {alert(err)})
     }
     return (
         <div className="mt-24">
---
+++ server/csvUpdater.js
@@ -0,0 +1,55 @@
+const csvWriter = require( 'csv-writer')
+const {join} = require('path');
+const fs = require('fs');
+const csvReader = require('csv-parser');
+
+const csvDir = join(process.cwd(), 'csvs')
+
+const createCsvWriter = csvWriter.createObjectCsvWriter;
+
+function getCSVData(id){
+    const realSlug = id.replace(/\.csv$/, '')
+    const fullPath = join(csvDir, `${realSlug}.csv`)
+    let data = {};
+  
+    const stream = fs.createReadStream(fullPath)
+    .pipe(csvReader())
+    .on('data', (row) => {
+      data[`${row?.Word}`] = row;
+    })
+  
+    return new Promise(resolve => {
+      stream.on('end', () => {
+        console.log('CSV file successfully processed');
+        resolve({
+            ...data
+        });
+      });
+    })
+    
+}
+
+module.exports = async function writeCsvData(params){
+    const id = params.id;
+    const realSlug = id.replace(/\.csv$/, '')
+    const fullPath = join(csvDir, `${realSlug}.csv`)
+  
+    const csvWriter = createCsvWriter({
+      path: fullPath,
+      header: [
+          {id: 'Test', title: 'Test'},
+          {id: 'Question', title: 'Question'},
+          {id: 'Word', title: 'Word'},
+          {id: 'Meaning', title: 'Meaning'},
+          {id: 'Sentence', title: 'Sentence'},
+          {id: 'POS', title: 'POS'},
+          {id: 'Deck', title: 'Deck'}
+      ]
+    });
+  
+    const csvData = await getCSVData(id);
+    csvData[params?.word]['Deck'] = params.deck;
+    const records = Object.keys(csvData).map((word)=> csvData[word]);
+  
+    return csvWriter.writeRecords(records)       // returns a promise
+}
